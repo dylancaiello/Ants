a1266bbc3e2a2895e34b0eed3c807cd084404034
dylancaiello <31040903+dylancaiello@users.noreply.github.com>
2025-08-11 10:05:07 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'cake-defense-ant-rush-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
