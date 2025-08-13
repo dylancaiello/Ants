fe34a9949a505f49c0453357e86265ef2324b733
dylancaiello <31040903+dylancaiello@users.noreply.github.com>
2025-08-13 10:22:13 -0700

    spawn scheduling

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SW v5 DEBUG
-const CACHE = 'ants-v5-DEBUG';
+// SW v6 DEBUG
+const CACHE = 'ants-v6-DEBUG';
 const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
