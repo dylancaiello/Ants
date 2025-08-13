5697f1a304b4bbfd1b15ca50bf3e7afa4a7275b9
dylancaiello <31040903+dylancaiello@users.noreply.github.com>
2025-08-13 12:46:53 -0700

    6.3

--- sw-v63.js
+++ sw-v63.js
@@ -1,5 +1,5 @@
-// SW v6.2 DEBUG (cache bump)
-const CACHE='ants-v6.2-DEBUG';
+// SW v6.3 DEBUG (new filename & cache key to break old control)
+const CACHE='ants-v6.3-DEBUG';
 const ASSETS=['./','./index.html','./manifest.json','./assets/ant.png','./assets/cake.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
