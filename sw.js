9c490c755733167acd9cea3baf5c9c618ee4f374
dylancaiello <31040903+dylancaiello@users.noreply.github.com>
2025-08-13 11:28:07 -0700

    added cake and ant art

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// SW v6 DEBUG
-const CACHE = 'ants-v6-DEBUG';
-const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
+// SW v6.1 DEBUG (cache bump)
+const CACHE='ants-v6.1-DEBUG';
+const ASSETS=['./','./index.html','./manifest.json','./assets/ant.png','./assets/cake.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
